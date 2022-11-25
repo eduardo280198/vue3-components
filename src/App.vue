6c12d352d234c8f2900e9c255b918fbcf5e1ee54
Eduardo Lozano <eduardo_280198@hotmail.com>
2022-11-24 18:00:06 -0600

    Props (defineProps)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
   import ButtonCounter from './components/ButtonCounter.vue';
+  import BlogPost from './components/BlogPost.vue'
 </script>
 
 <template>
-  <h1>APP</h1>
-  <ButtonCounter></ButtonCounter>
+  
+  <div class="container">
+    <h1>APP</h1>
+    
+    <ButtonCounter></ButtonCounter>
+    <button-counter></button-counter>
+
+    <BlogPost title="Post 1"></BlogPost>
+    <BlogPost title="Post 2"></BlogPost>
+    <BlogPost title="Post 3"></BlogPost>
+  </div>
 </template>
